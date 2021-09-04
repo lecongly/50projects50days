a87569be1133de53017fc0877c29e38966965b5b
Le Cong Ly <lecongly28102002@gmail.com>
2021-09-04 10:31:26 +0700

    day2

--- day2/app.js
+++ day2/app.js
@@ -32,7 +32,7 @@ function update() {
     ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
   if (currentActive === 1) {
     prev.disabled = true;
-  } else if (currentActive === 4) {
+  } else if (currentActive === circles.length) {
     next.disabled = true;
   } else {
     prev.disabled = false;
